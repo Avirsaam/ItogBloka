e8bba6a82a39fed4cea3964ad0fba8a2197cfbb7
AvirSaam <den.demchenko@gmail.com>
2023-02-26 16:15:00 +0300

    programm added

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Clear();
+
+const int shortWordLetterCount = 3;
+String [] array = {"hello", "2", "world", ":-)"};
+int shortWordCounter = 0;
+
+for (int i = 0; i < array.Length; i++)
+{    
+    if (array[i].Length <= shortWordLetterCount)
+    {        
+        shortWordCounter++;                
+    }
+}
+
+if (shortWordCounter > 0)
+{
+    String [] resultArray = new String[shortWordCounter];
+
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= shortWordLetterCount)
+        {
+            resultArray[--shortWordCounter] = array[i];            
+            Console.Write("\"" + array[i] + "\"");            
+            if (shortWordCounter == 0) Console.WriteLine("]");
+            else Console.Write(", ");
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"В исходном массиве нет слов меньше {shortWordLetterCount} букв");
+}
